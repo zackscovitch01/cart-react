6ab00d939e955556a74d339190efb819da01864e
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-06-11 06:55:23 -0400

    remove item

--- src/reducer.js
+++ src/reducer.js
@@ -10,6 +10,12 @@ const reducer = (state, action) => {
   if (action.type === CLEAR_CART) {
     return { ...state, cart: new Map() };
   }
+  if (action.type === REMOVE) {
+    const newCart = new Map(state.cart);
+    newCart.delete(action.payload.id);
+    return { ...state, cart: newCart };
+  }
+
   throw new Error(`No matching action type: ${action.type}`);
 };
 
