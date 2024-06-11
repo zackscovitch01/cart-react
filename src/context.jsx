38d8f0fc8a4a11f724d209a8f110ca9df890a4a3
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-06-11 06:15:06 -0400

    context setup

--- src/context.jsx
+++ src/context.jsx
@@ -1,3 +1,28 @@
 import { useContext, useReducer, useEffect, createContext } from "react";
+import reducer from "./reducer";
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from "./actions";
 
 const AppContext = createContext();
+
+const initialState = {
+  loading: false,
+  cart: [],
+};
+
+export const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
