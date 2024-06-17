d039bcf4f8d561b72d8ecbaacb2d5259a0b1aa4f
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-06-16 21:24:58 -0400

    .

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav>
       <div className="nav-center">
-        <h4>useReducer</h4>
+        <h4>LANDROID</h4>
         <div className="nav-container">
           <FaCartPlus className="cart-icon" />
           <div className="amount-container">
